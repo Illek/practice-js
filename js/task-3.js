184ac8ae723c467e701aafe68eb5f23de7fb31dd
Illek <dvrheim@gmail.com>
2025-02-17 00:47:26 +0200

    Updates

--- js/task-3.js
+++ js/task-3.js
@@ -1,5 +1,14 @@
 const a =
   "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Similique iusto sit ipsam, nesciunt ratione iure veniam commodi reprehenderit architecto officia eligendi asperiores provident aperiam. Reprehenderit quis quaerat vero similique aliquam modi, eligendi, doloremque sit enim voluptas neque incidunt fugit illum reiciendis! Sed corrupti tempore itaque culpa labore, veniam sapiente est!";
 
-console.log(a);
-consol;
+// console.log(a);
+// consol;
+
+// const value = "24.5px";
+// const numerical = Math.round(Number.parseFloat(value));
+// console.log(numerical);
+
+// console.log(Math.floor(Math.random() * 100) + 1);
+// console.log(Number.parseInt(Math.abs(Math.random() * 100)));
+
+// Оголошення параметрів x, y, z
